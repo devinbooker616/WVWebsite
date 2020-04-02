4df011e828ac6f7570077c5767b550fbc8d74016
Devin <dbooker@basecampcodingacademy.org>
2020-04-02 12:28:58 -0700

    Added apps and tinymce in INSTALLED_APPS
    Added tinymce to models and changed textbox to HTMLField

--- WVWebsite/app/models.py
+++ WVWebsite/app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 
 class Website(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-    textbox = models.TextField()
+    textbox = HTMLField()
--- WVWebsite/urls.py
+++ WVWebsite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 from WVWebsite.app.views import render_page
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", render_page, name="Home")
+    path("", render_page, name="Home"),
+    url(r'^tinymce/', include('tinymce.urls'))
 ]
