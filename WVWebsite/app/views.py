19600601e6d535e7dae7b11f0f713a0a83dad56b
Tyler Irving <tirving@basecampcodingacademy.org>
2020-04-02 14:05:35 -0500

    Initial

--- WVWebsite/app/views.py
+++ WVWebsite/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    if request.method == "GET":
+        return render(request, "templates/homepage.html")
--- WVWebsite/wsgi.py
+++ WVWebsite/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WVWebsite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WVWebsite.settings")
 
 application = get_wsgi_application()
