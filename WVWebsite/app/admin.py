9cdd5870356286252651318412a9351ccd8d7a85
Devin <dbooker@basecampcodingacademy.org>
2020-04-08 18:28:21 -0500

    registered whitelisted email

--- WVWebsite/app/admin.py
+++ WVWebsite/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from WVWebsite.app.models import Post, About
+from WVWebsite.app.models import Post, About, WhiteListedEmail
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,5 +10,10 @@ class AboutAdmin(admin.ModelAdmin):
     pass
 
 
+class WhiteListedEmailAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(About, AboutAdmin)
+admin.site.register(WhiteListedEmail, WhiteListedEmailAdmin)
